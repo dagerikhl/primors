8ba133a8d24a1b3b20daf5cb115838df36edea33
Løvgren, Dag Erik <dagerikhl@gmail.com>
2021-08-14 17:30:48 +0200

    Docs: Add README+CHANGELOG to SB, fix order of stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,20 @@
+import { getVersionedTitle } from "./utils/getVersionedTitle";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   backgrounds: {
     default: "light",
+  },
+  options: {
+    storySort: {
+      method: "alphabetical",
+      order: [
+        getVersionedTitle(),
+        ["Readme", "Changelog", "*"],
+        "Components",
+        "Hooks",
+        "*",
+      ],
+    },
   },
 };
